6b7d247be2cfd68c034d595193483f3e2d3f9308
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-07-26 22:36:12 +0100

    added snapshot, eventlistener and clear function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import {
   getDatabase,
   ref,
+  push,
+  onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -15,8 +18,55 @@ const endorsementsInDB = ref(database, "endorsementsList");
 
 const inputFieldEl = document.getElementById("input-field");
 const endorsementsBtnEl = document.getElementById("publish-btn");
+const endorsementListEl = document.getElementById("endorsements");
 
 endorsementsBtnEl.addEventListener("click", function () {
-  let inputValue = inputFieldEl;
-  console.log(inputFieldEl.value);
+  let inputValue = inputFieldEl.value;
+
+  push(endorsementsInDB, inputValue);
+
+  clearInputFieldEl();
 });
+
+onValue(endorsementsInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearEndorsementListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItemToEndorsementListEl(currentItem);
+    }
+  } else {
+    endorsementListEl.innerHTML = "No Endorsements Yet";
+  }
+});
+
+function clearEndorsementListEl() {
+  endorsementListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToEndorsementListEl(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("dblclick", function () {
+    let exactLocationOfItemInDB = ref(database, `endorsementsList/${itemID}`);
+
+    remove(exactLocationOfItemInDB);
+  });
+
+  endorsementListEl.append(newEl);
+}
